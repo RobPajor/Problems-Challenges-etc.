bf41d119484f90008fc771e760cefef9ed7a5166
Robert Pajor <50381813+RobPajor@users.noreply.github.com>
2020-09-10 09:24:15 +0200

    Update WriteOutNumbers.py

--- WriteOutNumbers.py
+++ WriteOutNumbers.py
@@ -1,4 +1,4 @@
-## Not finished yet
+## V.1.0.1 Code now works for 5 digit numbers
 
 numberDict = {0:"zero", 1: "one", 2: "two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten", 11: "eleven",
        12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen",
@@ -18,6 +18,12 @@ def number2words(n):
     
     if len(str(n)) == 3:
         return length_three(n)
+    
+    if len(str(n)) == 4:
+        return length_four(n)
+    
+    if len(str(n)) == 5:
+        return length_five(n)
 
           
 def lenght_two(n):
@@ -28,16 +34,42 @@ def lenght_two(n):
     if n < 20:
         return numberDict[n]
     sol = sol + numberDict[int(numberString[0] + "0")]
-    print(numberString)
     if numberString[1] != "0" and numberString[0] != "0" and n > 20:
-        print(sol)
         sol += "-" + numberDict[int(numberString[1])]
     return sol
 
 def length_three(n):
+    if n == 0:
+        return ""
+    
     numberString = str(n)
+    
+    if len(numberString) < 3:
+        return lenght_two(n)
+    
+    
     if lenght_two(int(numberString[1:3])) != "":
         hundreds = numberDict[int(numberString[0])] + " hundred " + lenght_two(int(numberString[1:3]))
     if lenght_two(int(numberString[1:3])) == "":
         hundreds = numberDict[int(numberString[0])] + " hundred" + lenght_two(int(numberString[1:3]))
     return hundreds
+
+def length_four(n):
+    numberString = str(n)
+    if length_three(int(numberString[1:4])) != "":
+        thousands = numberDict[int(numberString[0])] + " thousand " + length_three(int(numberString[1:4]))
+    else:
+        thousands = numberDict[int(numberString[0])] + " thousand"
+    return thousands
+
+def length_five(n):
+    numberString = str(n)
+    if length_three(int(numberString[2:5])) != "":
+        tenthousands = lenght_two(int(numberString[0:2])) + " thousand " + length_three(int(numberString[2:5]))
+    else:
+        tenthousands = lenght_two(int(numberString[0:2])) + "thousand"
+    return tenthousands
+
+    
+print(number2words(55543))
+    
